acf8e8fd3dff5217c9285ac9676904bdc09a0722
lius <100449511+liuslius@users.noreply.github.com>
2024-08-23 10:38:01 -0700

    children

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
-const Projects = ({ children }) => {
+const Projects = () => {
   return (
     <div
       className="flex flex-col items-center justify-center py-20"
