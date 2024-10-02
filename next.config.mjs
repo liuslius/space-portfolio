aabd7a603a3fa5712aa1cbcbcc7accb380743112
lius <100449511+liuslius@users.noreply.github.com>
2024-10-02 10:41:38 -0700

    production env

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
-  //basePath: process.env.NODE_ENV === "production" ? "/space-portfolio" : "",
-  //output: "export", // <=== enables static exports
+  //basePath: "",
+  basePath: process.env.NODE_ENV === "production" ? "/space-portfolio" : "",
+  output: "export", // <=== enables static exports
   images: { unoptimized: true },
   reactStrictMode: true,
 };
